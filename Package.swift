8782fd4966c7a04d8fc1c9765911c9ce246a5728
Diego Cid Merino <diegocidm4@hotmail.com>
2023-04-15 08:58:05 +0200

    Primera versión del framework protegido

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "iDNIe",
+    platforms: [
+        .macOS(.v12), .iOS(.v16)
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -16,13 +19,10 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "iDNIe",
-            dependencies: []),
-        .testTarget(
-            name: "iDNIeTests",
-            dependencies: ["iDNIe"]),
-    ]
+            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+            // Targets can depend on other targets in this package, and on products in packages this package depends on.
+            .binaryTarget(
+                name: "iDNIe",
+                path: "./Sources/iDNIe.xcframework")
+        ]
 )
