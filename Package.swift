0a612558c60eb41f787c1ed46f1dcc91e22e0cd2
Diego Cid Merino <diegocidm4@hotmail.com>
2024-07-01 20:35:57 +0200

    3.01.14

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Boilertalk/BigInt.swift.git", branch: "master"),
-        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", branch: "master"),
+        .package(url: "https://github.com/Boilertalk/BigInt.swift.git", exact: "1.0.0"),
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", exact: "1.1.2300"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", exact: "1.6.0")
     ],
     targets: [
