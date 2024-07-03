46e4b039c5e9b8e4b62d9a5f1792b6547a01f1d0
Diego Cid Merino <diegocidm4@hotmail.com>
2024-07-03 09:48:27 +0200

    3.01.16

--- Package.swift
+++ Package.swift
@@ -18,9 +18,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Boilertalk/BigInt.swift.git", exact: "1.0.0"),
-        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", exact: "1.1.2300")
-        //,
-        //.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", exact: "1.6.0")
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", exact: "1.1.2300"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", exact: "1.6.0")
     ],
     targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
